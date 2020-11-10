99e046cddaff50ccf9bb4cc82103002e533c0891
Maxim Leykin <Maxim_Leykin@epam.com>
2020-11-10 08:54:53 +0300

    Initial commit

---
+++ app/src/main/java/com/niit/android/service/RemoteService.java
@@ -0,0 +1,49 @@
+package com.niit.android.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.widget.Toast;
+
+public class RemoteService extends Service {
+	/** Command to the service to display a message */
+    static final int MSG_SAY_HELLO = 0;
+    static final int MSG_SAY_GOODBYE = 1;
+
+    /**
+     * Handler of incoming messages from clients.
+     */
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_SAY_HELLO:
+                    Toast.makeText(getApplicationContext(), "hello!", Toast.LENGTH_SHORT).show();
+                    break;
+                case MSG_SAY_GOODBYE:
+                    Toast.makeText(getApplicationContext(), "bye-bye!", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+
+    /**
+     * Target we publish for clients to send messages to IncomingHandler.
+     */
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
+
+    /**
+     * When binding to the service, we return an interface to our messenger
+     * for sending messages to the service.
+     */
+    @Override
+    public IBinder onBind(Intent intent) {
+        Toast.makeText(getApplicationContext(), "binding", Toast.LENGTH_SHORT).show();
+        return mMessenger.getBinder();
+    }
+}
